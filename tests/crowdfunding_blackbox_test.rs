063954cc00dedf23237a37897cc59d980d9c7e9c
mulleresposito <mulleresposito@gmail.com>
2025-04-06 17:54:40 +0000

    refact(CrowdfundingBlackboxTest):  - refacts the code to use the CrowdfundingTestState struct and methods.

--- tests/crowdfunding_blackbox_test.rs
+++ tests/crowdfunding_blackbox_test.rs
@@ -1,165 +1,105 @@
-use crowdfunding::crowdfunding_proxy::{self, Status};
+pub mod structs;
+
+use structs::crowdfunding_test_state::CrowdfundingTestState;
+use crowdfunding::crowdfunding_proxy::Status;
 use multiversx_sc_scenario::imports::*;
 
-const CODE_PATH: MxscPath = MxscPath::new("output/crowdfunding.mxsc.json");
-
-fn world() -> ScenarioWorld {
-    let mut blockchain = ScenarioWorld::new();
-
-    blockchain.set_current_dir_from_workspace("crowdfunding");
-    blockchain.register_contract(CODE_PATH, crowdfunding::ContractBuilder);
-    blockchain
-}
-
-const OWNER: TestAddress = TestAddress::new("owner");
+const OWNER_ADDRESS: TestAddress = TestAddress::new("owner");
 const CROWDFUNDING_ADDRESS: TestSCAddress = TestSCAddress::new("crowdfunding");
-const DONOR: TestAddress = TestAddress::new("donor");
+const DONOR_ONE_ADDRESS: TestAddress = TestAddress::new("donor_one");
+const DONOR_TWO_ADDRESS: TestAddress = TestAddress::new("donor_two");
+const DONOR_ONE_DONATION: u64 = 100_000_000_000;
+const DONOR_TWO_DONATION: u64 = 200_000_000_000;
 
 const TARGET: u64 = 200_000_000_000;
 const DEADLINE: u64 = 123_000;
 const AFTER_DEADLINE: u64 = 123_001;
-const DONOR_INITIAL_BALANCE: u64 = 500_000_000_000;
-const DONOR_INITIAL_DEPOSIT: u64 = 150_000_000_000;
-
-fn crowdfunding_deploy() -> ScenarioWorld {
-    let mut world = world();
-    
-    world.account(OWNER).nonce(0).balance(1000000);    
-    
-    let crowdfunding_address = world
-        .tx()
-        .from(OWNER)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .constructor(TARGET, DEADLINE)
-        .code(CODE_PATH)
-        .new_address(CROWDFUNDING_ADDRESS)
-        .returns(ReturnsNewAddress)
-        .run();
-
-    assert_eq!(crowdfunding_address, CROWDFUNDING_ADDRESS.to_address());
-
-    world
-}
+const INITIAL_BALANCE: u64 = 300_000_000_000;
 
 #[test]
 fn crowdfunding_deploy_test() {
-    let mut world: ScenarioWorld = crowdfunding_deploy();
+    let mut state = CrowdfundingTestState::new();
+    
+    state.set_balance(OWNER_ADDRESS, INITIAL_BALANCE);
 
-    world.check_account(OWNER).balance(1_000_000);
+    state.deploy(OWNER_ADDRESS, TARGET, DEADLINE);
 
-    world
-        .query()
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .target()
-        .returns(ExpectValue(TARGET))
-        .run();
+    state.world.check_account(OWNER_ADDRESS).balance(INITIAL_BALANCE);
 
-    world
-        .query()
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .deadline()
-        .returns(ExpectValue(DEADLINE))
-        .run();
-}
+    state.check_target(TARGET);
 
-
-fn crowdfunding_fund() -> ScenarioWorld {
-    let mut world: ScenarioWorld = crowdfunding_deploy();
-
-    world.account(DONOR).nonce(0).balance(DONOR_INITIAL_BALANCE);
-
-    world
-        .tx()
-        .from(DONOR)
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .fund()
-        .egld(DONOR_INITIAL_DEPOSIT)
-        .run();
-
-    world
+    state.check_deadline(DEADLINE);
 }
 
 #[test]
 fn crowdfunding_fund_test() {
-    let mut world: ScenarioWorld = crowdfunding_fund();
+    let mut state = CrowdfundingTestState::new();
 
-    world.check_account(OWNER).nonce(1).balance(1_000_000u64);
-    world.check_account(DONOR).nonce(1).balance(DONOR_INITIAL_BALANCE - DONOR_INITIAL_DEPOSIT);
-    world.check_account(CROWDFUNDING_ADDRESS).nonce(0).balance(DONOR_INITIAL_DEPOSIT);
+    state.set_balance(OWNER_ADDRESS, INITIAL_BALANCE);
+    state.deploy(OWNER_ADDRESS, TARGET, DEADLINE);
 
-    world
-        .query()
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .target()
-        .returns(ExpectValue(TARGET))
-        .run();
+    state.set_balance(DONOR_ONE_ADDRESS, INITIAL_BALANCE);
 
-    world
-        .query()
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .deadline()
-        .returns(ExpectValue(DEADLINE))
-        .run();
+    state.world.check_account(OWNER_ADDRESS).nonce(1).balance(INITIAL_BALANCE);
+    state.world.check_account(DONOR_ONE_ADDRESS).nonce(0).balance(INITIAL_BALANCE);
 
-    world
-        .query()
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .deposit(DONOR)
-        .returns(ExpectValue(DONOR_INITIAL_DEPOSIT))
-        .run();
+    
+    state.fund(DONOR_ONE_ADDRESS, DONOR_ONE_DONATION);
+
+    state.world
+        .check_account(DONOR_ONE_ADDRESS)
+        .nonce(1)
+        .balance(INITIAL_BALANCE - DONOR_ONE_DONATION);
+    
+    state.world
+        .check_account(CROWDFUNDING_ADDRESS)
+        .nonce(0)
+        .balance(DONOR_ONE_DONATION);
+
+    state.check_target(TARGET);
+
+    state.check_deadline(DEADLINE);
+
+    state.check_deposit(DONOR_ONE_ADDRESS, DONOR_ONE_DONATION);
 }
 
 #[test]
 fn crowdfunding_fund_too_late_test() {
-    let mut world: ScenarioWorld = crowdfunding_fund();
+    let mut state = CrowdfundingTestState::new();
 
-    world.current_block().block_timestamp(AFTER_DEADLINE);
+    state.set_balance(OWNER_ADDRESS, INITIAL_BALANCE);
 
-    world
-        .query()
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .status()
-        .returns(ExpectValue(Status::Failed))
-        .run();
+    state.deploy(OWNER_ADDRESS, TARGET, DEADLINE);
+
+    state.set_balance(DONOR_ONE_ADDRESS, INITIAL_BALANCE);
+
+    state.world.current_block().block_timestamp(AFTER_DEADLINE);
+
+    state.check_status(Status::Failed);
 }
 
 #[test]
 fn crowdfunding_claim_test() {
-    let mut world: ScenarioWorld = crowdfunding_fund();
+    let mut state = CrowdfundingTestState::new();
 
-    world
-        .tx()
-        .from(DONOR)
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .fund()
-        .egld(100_000_000_000u64)
-        .run();
+    state.set_balance(OWNER_ADDRESS, INITIAL_BALANCE);
 
-    world.current_block().block_timestamp(AFTER_DEADLINE);
+    state.deploy(OWNER_ADDRESS, TARGET, DEADLINE);
 
-    world
-        .query()
-        .to(CROWDFUNDING_ADDRESS)
-        .typed(crowdfunding_proxy::CrowdfundingProxy)
-        .status()
-        .returns(ExpectValue(Status::Successful))
-        .run();
+    state.set_balance(DONOR_ONE_ADDRESS, INITIAL_BALANCE);
+    state.set_balance(DONOR_TWO_ADDRESS, INITIAL_BALANCE);
 
-    // world
-    //     .tx()
-    //     .from(DONOR)
-    //     .to(CROWDFUNDING_ADDRESS)
-    //     .typed(crowdfunding_proxy::CrowdfundingProxy)
-    //     .claim()
-    //     .with_result(ExpectError(4, "Only owner can claim successfuly funds"))
-    //     .run();
+    state.check_status(Status::Funding);
 
+    state.fund(DONOR_ONE_ADDRESS, DONOR_ONE_DONATION);
+
+    state.fund(DONOR_TWO_ADDRESS, DONOR_TWO_DONATION);
+
+    state.world.current_block().block_timestamp(AFTER_DEADLINE);
+
+    state.check_status(Status::Successful);
+    
+    state.claim_by_not_owner(DONOR_ONE_ADDRESS);
+
+    state.claim_by_owner(OWNER_ADDRESS);
 }
